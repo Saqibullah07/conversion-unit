c64d0acd1e3ec44cfae41b889abcb22a4d380885
Saqibullah07 <81403548+Saqibullah07@users.noreply.github.com>
2021-03-26 10:31:08 +0500

    Add files via upload

---
+++ Unit Conversion 2.cpp
@@ -0,0 +1,39 @@
+    #include <iostream>
+     using namespace std;
+     int main (){
+	
+	double milli_meter,kilo_meter;
+	double result;
+	int check;
+	
+	
+	 
+    do
+    
+	{
+	
+	
+	cout<<"Enter Amount To Convert"<<endl;
+	cin>>milli_meter;
+	
+	result =milli_meter*1/1000;
+	
+	cout<<"Convert From milli Meter "<<endl;
+	cout<<"Convert From to Meter"<<endl;
+	cout<<"Convertion Amount Is "<<milli_meter<<endl;
+	
+	cout<<"The Result Is "<<result<<endl;
+	
+	
+	
+	cout<<"Press 1 to Continue"<<endl;
+		cin>>check;
+	
+		
+	}
+		while(check==1);
+		
+	}
+	
+	
+
